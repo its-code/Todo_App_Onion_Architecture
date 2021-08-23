937f9126b72b90e5de79f1928527fc7e9b024cce
Asad Raza <asad.raza@carbonteq.com>
2021-08-23 20:59:01 +0500

    Integrated Config ENV for Security Purpose

--- http/controllers/user.controller.js
+++ http/controllers/user.controller.js
@@ -1,4 +1,4 @@
-const user = require("../../models/user")
+const user = require("../../db/models/user")
 const User = require("../../services/user.service")
 class UserController{
 
--- http/middleware/auth.js
+++ http/middleware/auth.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken")
-const User = require("../../models/user")
+const User = require("../../db/models/user")
 
 const auth =async (req,res,next)=>{
     try{
